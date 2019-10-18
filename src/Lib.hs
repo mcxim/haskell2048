83363363174aa49a5d8993de5cde790ea8e22685
mcxim <gelfandmaxim@gmail.com>
2019-10-18 21:33:30 +0300

    Add function descriptions

--- src/Lib.hs
+++ src/Lib.hs
@@ -21,15 +21,18 @@ type Board = [[Int]]
 nCols = 4 :: Int
 nRows = 4 :: Int
 
+-- | Returns board with changed cell.
 changePos :: Int -> Int -> Int -> Board -> Board
 changePos row col val board =
   take row board
     ++ [take col (board !! row) ++ [val] ++ drop (col + 1) (board !! row)]
     ++ drop (row + 1) board
 
+-- | Returns True if the board contains a 2048 tile.
 checkWin :: Board -> Bool
 checkWin = any (elem 2048)
 
+-- | Returns True when if there are no more possible moves.
 checkLose :: Board -> Bool
 checkLose board =
   (not . any (elem 0)) board
@@ -42,21 +45,25 @@ checkLose board =
     && move Dwn board
     == board
 
+-- | Returns the row, merged to the left.
 merge :: [Int] -> [Int]
 merge lst = take (length lst) $ merge' (filter (/= 0) lst) ++ repeat 0
 
+-- | Helper function for merge, taking tiles without empty spaces.
 merge' :: [Int] -> [Int]
 merge' []  = []
 merge' [a] = [a]
 merge' (x : y : xs) | x == y    = (2 * x) : merge' xs
                     | otherwise = x : merge' (y : xs)
 
+-- | Returns the board afte commiting the given move.
 move :: Move -> Board -> Board
 move Lft = map merge
 move Rgt = map (reverse . merge . reverse)
 move Upp = transpose . move Lft . transpose
 move Dwn = transpose . move Rgt . transpose
 
+-- | Returns a string representation of a number.
 myShow :: Int -> String
 myShow num | len == 1 = "  " ++ shown ++ "  "
            | len == 2 = " " ++ shown ++ "  "
@@ -66,12 +73,16 @@ myShow num | len == 1 = "  " ++ shown ++ "  "
   shown = if num == 0 then "_" else show num
   len   = length shown
 
+-- | Prints the current state of the board.
 prettyPrint :: Board -> IO ()
 prettyPrint =
   putStrLn . unlines . map unwords . intersperse [] . (map . map) myShow
 
+-- | Just an empty board, no tiles at all.
+emptyBoard :: Board
 emptyBoard = [ [ 0 | col <- [1 .. nCols] ] | row <- [1 .. nRows] ] :: Board
 
+-- | Returns the board with an added random tile if there is space.
 addRandom :: Board -> IO Board
 addRandom board =
   let emptyCells =
@@ -91,10 +102,11 @@ addRandom board =
           (row, col) <- chosenOne
           return $ changePos row col val board
 
+-- | Returns a board with two random tiles.
 newBoard :: IO Board
 newBoard = (addRandom >=> addRandom) emptyBoard
 
-
+-- | The main game loop, with input and output.
 gameLoop :: Board -> IO ()
 gameLoop board
   | checkWin board = putStrLn "You won! congrats."
@@ -121,6 +133,7 @@ gameLoop board
           prettyPrint nextBoard
           gameLoop nextBoard
 
+-- | The main funtion.
 runGame :: IO ()
 runGame = do
   putStrLn "Use hjkl or wasd keys to operate, q to quit."
